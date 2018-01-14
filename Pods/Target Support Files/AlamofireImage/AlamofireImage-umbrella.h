479bb739a185d72d075178dbff25684d3ffc3ca5
Chaithat Sukra <iChaithat@Chaithats-MacBook-Pro.local>
2018-01-14 12:35:15 +0700

    a

--- Pods/Target Support Files/AlamofireImage/AlamofireImage-umbrella.h
+++ Pods/Target Support Files/AlamofireImage/AlamofireImage-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_SenditProjectVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SenditProjectVersionString[];
+FOUNDATION_EXPORT double AlamofireImageVersionNumber;
+FOUNDATION_EXPORT const unsigned char AlamofireImageVersionString[];
 
--- Pods/Target Support Files/Pods-SenditProject/Pods-SenditProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SenditProject : NSObject
-@end
-@implementation PodsDummy_Pods_SenditProject
-@end
